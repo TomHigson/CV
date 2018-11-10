bcef57bac2686aae395852493eb685cbe3dfa6b1
Thomas Higson <thomas.higson@gmail.com>
2018-11-10 17:21:16 +0000

    updated imports to include card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
 import {NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -13,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
