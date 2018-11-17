20270c1d3e7341d7c7626db00bed382f41df3ad5
Thomas Higson <thomas.higson@gmail.com>
2018-11-17 14:38:33 +0000

    Switched from css to sass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {CvService} from './cv.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   pageTitle = '';
