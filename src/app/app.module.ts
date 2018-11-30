2c04fef33df45bb0b0ae56a3a570f7f08a8701a8
Thomas Higson <thomas.higson@gmail.com>
2018-11-30 19:32:52 +0000

    Modified spacing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
-import {BrowserModule, Title} from '@angular/platform-browser';
+import {BrowserModule, Title}    from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule}                from '@angular/core';
 
-import {NgModule} from '@angular/core';
+import {HttpClientModule}        from '@angular/common/http';
 
-import {HttpClientModule} from '@angular/common/http';
+import {MatButtonModule}         from '@angular/material';
+import {MatCardModule}           from '@angular/material/card';
+import {MatToolbarModule}        from '@angular/material/toolbar';
+import {MatDividerModule}        from '@angular/material/divider';
+import {MatListModule}           from '@angular/material/list';
+import {MatSelectModule}         from '@angular/material/select';
+import {MatIconModule}           from '@angular/material/icon';
 
-import {MatButtonModule} from '@angular/material';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import {MatSelectModule} from '@angular/material/select';
-import {MatIconModule} from '@angular/material/icon';
+import {FlexLayoutModule}        from '@angular/flex-layout';
 
-import {FlexLayoutModule} from '@angular/flex-layout';
-
-import {AppComponent} from './app.component';
-import {JobComponent} from './job/job.component';
-import {RoleComponent} from './role/role.component';
-import {SkillComponent} from './skill/skill.component';
+import {AppComponent}            from './app.component';
+import {JobComponent}            from './job/job.component';
+import {RoleComponent}           from './role/role.component';
+import {SkillComponent}          from './skill/skill.component';
 
 @NgModule({
   declarations: [
